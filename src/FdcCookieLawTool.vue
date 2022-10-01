f1757856d53fca30c66069c0ae6e64a3c7a637c9
Gix075 <gildo.giuliani@gmail.com>
2022-10-01 16:43:19 +0200

    tool elements style
    Mobile style

--- src/FdcCookieLawTool.vue
+++ src/FdcCookieLawTool.vue
@@ -33,11 +33,16 @@
     <div v-if="page === 'cookies' || page === 'cookie'" class="fdc-policypage cookie-policy">
         <h1>Cookie Policy</h1>
         <div class="fdc--choises-panel">
-            <h2>Configurazione delle scelte</h2>
-            <p>
-                Da questo pannello puoi abilitare e/o disabilitare i cookie di terze parti in uso in questo sito.<br>
-                Per saperne di più puoi leggere l'informativa estesa che trovi sotto questo pannello.
+            
+            <div class="fdc--choises-panel--intro">
+                <h2>Configurazione delle scelte</h2>
+                <p>
+                    Da questo pannello puoi abilitare e/o disabilitare i cookie di terze parti in uso in questo sito.<br>
+                    Per saperne di più puoi leggere l'informativa estesa che trovi sotto questo pannello.
                 </p>
+            </div>
+            
+
             <div v-for="servicecat in tool.config_cookie_policy.services" :key="servicecat.catName" class="fdc--choises-panel--services-category">
                 <h3>{{servicecat.catLabel}}</h3>
                 <div v-for="(service,index) in servicecat.services" :key="index" :data-service="index" class="fdc--choises-panel--service">
@@ -92,6 +97,10 @@
                 default: "none"
             },
             "banner": {
+                type: Boolean,
+                default: false
+            },
+            "style": {
                 type: Boolean,
                 default: false
             }
@@ -241,25 +250,116 @@
 
 <style lang="scss">
 
+    @mixin clors_schema($schema:"base") {
+
+        @if $schema == "base" {
+
+            // Coolors
+            --fdc-color--black: #03191E;
+            --fdc-color--gray-extra-light: #efefef;
+            --fdc-color--white: #fff;
+            --fdc-color--green: #42b983;
+            --fdc-color--red: #f02d3a;
+            --fdc-color--orange: #F18F01;
+
+            // Buttons
+            --fdc-color--button-accept--background: var(--fdc-color--green);
+            --fdc-color--button-accept--color: var(--fdc-color--white);
+            --fdc-color--button-reject--background: var(--fdc-color--red);
+            --fdc-color--button-reject--color: var(--fdc-color--white);
+            --fdc-color--button-customize--background: var(--fdc-color--orange);
+            --fdc-color--button-customize--color: var(--fdc-color--white);
+        }
+
+        @if $schema == "light" {
+
+            // Banner
+            --fdc-color--banner-background: var(--fdc-color--black);
+            --fdc-color--banner-color: var(--fdc-color--white);
+            
+            // Close Icon Button
+            --fdc-color--button-close--background: var(--fdc-color--white);
+            --fdc-color--button-close--color: var(--fdc-color--black);
+
+            // Services Panels
+            --fdc-color--panel-bg: var(--fdc-color--black);
+            --fdc-color--panel-color: var(--fdc-color--white);
+            --fdc-color--panel-border: var(--fdc-color--black);
+
+            // Borders
+            --fdc-color--border-color: var(--fdc-color--gray-extra-light);
+
+        }
+
+        @if $schema == "dark" {
+            
+            // Banner
+            --fdc-color--banner-background: var(--fdc-color--white);
+            --fdc-color--banner-color: var(--fdc-color--black);
+            
+            // Close Icon Button
+            --fdc-color--button-close--background: var(--fdc-color--black);
+            --fdc-color--button-close--color: var(--fdc-color--white);
+
+            // Services Panels
+            --fdc-color--panel-bg: var(--fdc-color--white);
+            --fdc-color--panel-color: var(--fdc-color--black);
+            --fdc-color--panel-border: var(--fdc-color--black);
+
+            // Borders
+            --fdc-color--border-color: var(--fdc-color--black);
+
+        }
+        
+    }
+
+    :root {
+
+        @include clors_schema();
+        @include clors_schema("light");
+
+    }
+
+    @media (prefers-color-scheme: dark) {
+        :root {
+            @include clors_schema("dark");
+        }
+    }
+
+    body.dark {
+        @include clors_schema("dark");
+    }
+
     .fdc--choises-panel {
         
         text-align: left;
+
+        .fdc--choises-panel--intro {
+            padding-top: 20px;
+            margin-bottom: 40px;
+        }
         
         .fdc--choises-panel--services-category {
             margin-bottom: 60px;
             padding: 40px;
-            //border: 1px solid #efefef;
             border-radius: 20px;
-            background-color: #03191E;
+            background-color: var(--fdc-color--panel-bg);
+            color: var(--fdc-color--panel-color);
             > h3 {
-                font-size: 2em;
+                font-size: 1.5rem;
+                line-height: 1em;
                 margin: 0 0 20px 0;
+            }
+            @media all and (min-width: 768px) {
+                > h3 {
+                    font-size: 2rem;
+                }
             }
         }
 
 
         .fdc--choises-panel--service {
-            border-bottom: 1px solid #efefef;
+            border-bottom: 1px solid var(--fdc-color--border-color);
             display: flex;
             flex-wrap: wrap;
             align-items: center;
@@ -273,7 +373,9 @@
             }
 
             .fdc--choises-panel--service-text {
-                width: 50%;
+                
+                width: 100%;
+                margin-bottom: 5px;
 
                 .fdc--choises-panel--service-policy {
                     
@@ -299,7 +401,7 @@
 
             .fdc--choises-panel--service-buttons {
                 text-align: right;
-                width: 50%;
+                width: 100%;
                 &.service--accepted {
                     .button--accept {
                         opacity: 0.5;
@@ -313,6 +415,26 @@
                         pointer-events: none;
                     }
                 }
+
+                @media all and (max-width: 767px) {
+                    display: flex;
+                    margin: 0 -3px;
+                    .fdc-button-wrapper {
+                        width: calc(50% - 6px);
+                        margin: 0 3px;
+                        .fdc-button {
+                            width: 100%;
+                        }
+                    }
+                }
+            }
+
+            @media all and (min-width: 768px) {
+                .fdc--choises-panel--service-text,
+                .fdc--choises-panel--service-buttons {
+                    width: 50%;
+                    margin-bottom: 0;
+                }
             }
         }
 
@@ -322,16 +444,21 @@
     /* BANNER */
     .fdc-banner {
         
-        background-color: #03191E;
+        background-color: var(--fdc-color--banner-background);
+        color: var(--fdc-color--banner-color);
         text-align: left;
-        padding: 40px;
+        padding: 40px 20px 20px;
         position: fixed;
         z-index: 100;
         left: 50%;
         right: auto;
+        width: 90%;;
         max-width: 800px;
         transform: translateX(-50%);
         border-radius: 20px;
+        font-size: 0.7rem;
+
+        
 
         .fdc-banner--text {
             margin-bottom: 40px;
@@ -349,14 +476,17 @@
 
         .fdc-banner--close {
             position: absolute;
-            top: 20px;
-            right: 20px;
-            width: 25px;
-            height: 25px;
-            background: white;
-            color: black;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            top: -32px;
+            right: -10px;
+            width: 30px;
+            height: 30px;
+            background: var(--fdc-color--button-close--background);
+            color: var(--fdc-color--button-close--color);
             text-align: center;
-            line-height: 25px;
+            font-size: 1rem;
             border-radius: 50%;
             cursor: pointer;
             transition: all .2s ease;
@@ -366,9 +496,42 @@
             }
         }
 
+        .fdc-buttons {
+            display: flex;
+            flex-wrap: wrap;
+            margin: 0 -3px;
+        }
+
         &.banner--bottom {
             bottom: 20px;
             top: auto;
+        }
+
+        @media all and (max-width: 767px) {
+            .fdc-button-wrapper {
+                width: calc(50% - 6px);
+                .fdc-button {
+                    width: 100%;
+                    text-align: center;
+                }
+                &.button--accept {
+                    width: calc(100% - 6px);
+                }
+
+            }
+        }
+
+
+        @media all and (min-width: 768px) {
+            
+            font-size: 1rem;
+            padding: 40px;
+
+            .fdc-banner--close {
+                top: -20px;
+                right: -20px;
+            }
+
         }
         
     }
@@ -384,8 +547,8 @@
     button.fdc-button,
     a.fdc-button {
         display: block;
-        border: 1px solid white;
-        color: white;
+        border-width: 1px;
+        border-style: solid;
         font-size: 1em;
         line-height: 1em;
         padding: 10px 20px;
@@ -395,18 +558,21 @@
         transition: all .2s ease;
 
         &.button--accept {
-            background-color: #42b983;
-            border-color: #42b983;
+            background-color: var(--fdc-color--button-accept--background);
+            border-color: var(--fdc-color--button-accept--background);
+            color: var(--fdc-color--button-accept--color);
         }
 
         &.button--reject {
-            background-color: #f02d3a;
-            border-color: #f02d3a;
+            background-color: var(--fdc-color--button-reject--background);
+            border-color: var(--fdc-color--button-reject--background);
+            color: var(--fdc-color--button-reject--color);
         }
 
         &.button--customize {
-            background-color: #F18F01;
-            border-color: #F18F01;
+            background-color: var(--fdc-color--button-customize--background);
+            border-color: var(--fdc-color--button-customize--background);
+            color: var(--fdc-color--button-customize--color);
         }
 
         &:hover {
